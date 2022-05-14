e7fbcdbc5cb851dc4cc593d274b9e9f0b78c5127
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-14 12:32:37 -0500

    Implementando la función de invertir arreglo e imprimiendo arreglo invertido

--- invertirMatriz.java
+++ invertirMatriz.java
@@ -1,13 +1,21 @@
 public class invertirMatriz {
     public static void main (String[] args) {
-	int[] a = { 1, 2, 3};
+	int[] a = { 1, 2, 3, 4, 5, 6};
 	imprimirArray(a);	
+	int[] aInv = invertirArray(a);
+	imprimirArray(aInv);	
     }
 
-   //public static int[] invertirArray(int[] A){
+   public static int[] invertirArray(int[] A){
     //Procedimiento para invertir la matriz
-		
-	//return 0;
+	int Ain[] = new int[A.length];	
+	int j = 0;
+	for (int i = A.length - 1; i >= 0; i--) {
+	    Ain[j] = A[i];
+	    j++;
+	}
+	return Ain;
+    }
     
 
     public static void imprimirArray(int[] array) {
