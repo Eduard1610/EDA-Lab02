2e44bd35ee4855d0b7469b729e1f284c37e4632c
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-14 13:57:39 -0500

    Implementando n veces la rotación de la matriz

--- rotarIzquierda.java
+++ rotarIzquierda.java
@@ -1,41 +1,44 @@
 import java.util.Scanner;
 
 public class rotarIzquierda {
-    static int numRotaciones; 
-    public static void main (String[] args) {
+    static int numRotaciones;
+
+    public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	int[] a = { 1, 2, 3, 4, 5, 6};
+	int[] a = { 1, 2, 3, 4, 5, 6 };
 	imprimirArray(a);
 
 	System.out.print("Ingrese el número de rotaciones a la izquierda: ");
 	numRotaciones = sc.nextInt();
-	rotar1Izquierda(a);
-	
+	int[] arregloInvertido = rotarIzquierda(a);
+	imprimirArray(arregloInvertido);
+
     }
-    
-    /*public int[] rotarIzquierdaArray(int[] A){
-	//Procedimiento para rotar la matriz
-	return Aiz;
-    }*/
 
-    public static int[] rotar1Izquierda(int[] A){
-	//Procedimiento para clonar la matriz A en Aiz
-	int[] Aiz = new int[A.length]; 
-	for(int i = 0; i < A.length; i++) {
-	    Aiz[i] = A[i];
-	}
-	imprimirArray(Aiz);
-
-	//Procedimiento para rotar la matriz 1 posición izquierda
+    public static void rotar1Izquierda(int[] Aiz) {
+	// Procedimiento para rotar la matriz 1 posición izquierda
 	int primero = Aiz[0];
 	for (int i = 0; i < Aiz.length - 1; i++) {
-	    Aiz[i] = Aiz[i+1];
+	    Aiz[i] = Aiz[i + 1];
 	}
 	Aiz[Aiz.length - 1] = primero;
-	return Aiz;
     }
 
+    public static int[] rotarIzquierda(int[] A) {
+	// Procedimiento para clonar la matriz A en Aiz
+	int[] Aiz = new int[A.length];
+	for (int i = 0; i < A.length; i++) {
+	    Aiz[i] = A[i];
+	}
 
+	//Rotar n veces el arreglo a la izquierda
+	for (int i = 0; i < numRotaciones; i++) {
+	    rotar1Izquierda(Aiz);
+	}
+
+	return Aiz;
+    }
+    
     public static void imprimirArray(int[] array) {
 	System.out.print("[ ");
 	for (int i = 0; i < array.length; i++) {
