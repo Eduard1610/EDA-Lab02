c48f3e8783fa249421a055fbf10e61709d415656
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-14 12:24:24 -0500

    Agregando función para imprimir arreglo

--- invertirMatriz.java
+++ invertirMatriz.java
@@ -1,12 +1,20 @@
 public class invertirMatriz {
     public static void main (String[] args) {
 	int[] a = { 1, 2, 3};
-
+	imprimirArray(a);	
     }
 
-    public int[] invertirArray(int[] A){
+   //public static int[] invertirArray(int[] A){
     //Procedimiento para invertir la matriz
 		
-	return Ain;
+	//return 0;
+    
+
+    public static void imprimirArray(int[] array) {
+	System.out.print("[ ");
+	for (int i = 0; i < array.length; i++) {
+	    System.out.print(array[i] + " ");
+	}
+	System.out.println("]");
     }
 }
