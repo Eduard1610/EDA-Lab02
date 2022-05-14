43ca98b8346309fb429bea1ca5596dae6a080b79
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-14 12:18:28 -0500

    Creando plantilla de el ejercicio

---
+++ invertirMatriz.java
@@ -0,0 +1,12 @@
+public class invertirMatriz {
+    public static void main (String[] args) {
+	int[] a = { 1, 2, 3};
+
+    }
+
+    public int[] invertirArray(int[] A){
+    //Procedimiento para invertir la matriz
+		
+	return Ain;
+    }
+}
