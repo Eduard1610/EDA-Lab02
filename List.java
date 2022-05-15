df094b234187b4a79ef2ce4fec17c7f237f738ae
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-05-15 01:27:11 -0500

    Funcion que añade un nodo al inicio de la lista

--- List.java
+++ List.java
@@ -16,5 +16,14 @@ public class List<T>
 	public void setRoot(Node<T> root) {
 		this.root = root;
 	}
-
+  // inserta un nodo al inicio de la lista
+  public void insertarInicio(T data)	{
+		if (this.root == null)//Si la tabla esta vacia
+			this.root = new Node<T>(_data);//Crea un nodo, almacena el valor de data y lo asigna como nodo de cabecera
+		else {
+			Node<T> newNode = new Node<T>(_data);//crea un nodo
+			newNode.setNext(root);//Vincula el nodo cabecera a newNode
+			setRoot(newNode);//Asigna como nodo de cabecera a newNode
+		}
+	}
 }
