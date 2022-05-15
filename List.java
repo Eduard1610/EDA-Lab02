915c9ce60662c49917efedaf64ae1c0e65fa9e9b
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-05-15 01:39:31 -0500

    Funcion que añade un nodo al final de la lista

--- List.java
+++ List.java
@@ -19,11 +19,25 @@ public class List<T>
   // inserta un nodo al inicio de la lista
   public void insertarInicio(T data)	{
 		if (this.root == null)//Si la tabla esta vacia
-			this.root = new Node<T>(_data);//Crea un nodo, almacena el valor de data y lo asigna como nodo de cabecera
+			this.root = new Node<T>(data);//Crea un nodo, almacena el valor de data y lo asigna como nodo de cabecera
 		else {
-			Node<T> newNode = new Node<T>(_data);//crea un nodo
+			Node<T> newNode = new Node<T>(data);//crea un nodo
 			newNode.setNext(root);//Vincula el nodo cabecera a newNode
 			setRoot(newNode);//Asigna como nodo de cabecera a newNode
+		}
+	}
+
+  // Inserta un nodo al final
+	public void insertarAlFinal(T data)	{
+		if (this.root == null)
+			this.root = new Node<T>(data);
+		else {
+			Node<T> newNode = new Node<T>(data);//crea un nodo
+			Node<T> nodoActual = root;
+			while(nodoActual.getNext() != null)	{//bucle que recorre nodo por nodo
+				nodoActual = nodoActual.getNext();//asigna a nodoActual el nodo siguiente
+			}
+			nodoActual.set(newNode);//Cuando llega al nodo final asigna a nodoActual como el siguiente nodo vinculado
 		}
 	}
 }
